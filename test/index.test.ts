e64141c0b7972ce1da644589d976342164d5153c
Allan Verde <allangreen2906@gmail.com>
2023-06-19 18:09:34 -0300

    feat: crud dos hoteis com testes criados

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,78 @@
 import axios from 'axios'
-import { expect, test } from 'vitest'
+import { describe, expect, it, test } from 'vitest'
 
-// given, when, then - arrange, act, assert
 test('Deve listar todas os hoteis', async () => {
     const response = await axios({
         url: `http://localhost:8000/hotels`,
-        method: 'GET'
+        method: 'GET',
+        validateStatus: () => true
     })
     const hoteis = response.data
-    expect(hoteis.length).toBe(5)
+    expect(hoteis.length).not.toBe(0)
+})
+
+test('Deve criar um novo hotel', async () => {
+    const response = await axios({
+        url: `http://localhost:8000/hotels`,
+        method: 'POST',
+        data: {
+            name: 'Hotel Teste',
+            city: 'São Paulo'
+        },
+        validateStatus: () => true
+    })
+    const novoHotel = response.data
+    expect(novoHotel.name).toBe('Hotel Teste')
+    expect(novoHotel.city).toBe('São Paulo')
+})
+
+describe('Testes de mostragem da rota /hotels/:id', () => {
+  it('Deve retornar um hotel existente por ID', async () => {
+    const id = 1
+    const response = await axios({
+        url: `http://localhost:8000/hotels/${id}`,
+        method: 'GET',
+        validateStatus: () => true
+    })
+
+    expect(response.status).toBe(200)
+    expect(response.data.id).toBe(id)
+  })
+
+  it('Deve retornar erro 404 para um hotel inexistente', async () => {
+    const id = 999
+    const response = await axios({
+        url: `http://localhost:8000/hotels/${id}`,
+        method: 'GET',
+        validateStatus: () => true
+    })
+
+    expect(response.status).toEqual(404)
+    expect(response.data.message).toBe('Hotel não encontrado')
+  })
+})
+
+describe('Testes de exclusão da rota /hotels/:id', () => {
+    it('Deve excluir um hotel existente por ID', async () => {
+      const id = 6
+      const response = await axios({
+          url: `http://localhost:8000/hotels/${id}`,
+          method: 'DELETE',
+          validateStatus: () => true
+      })
+  
+      expect(response.status).toBe(204)
+    })
+  
+    it('Deve retornar erro 404 para um hotel inexistente', async () => {
+      const id = 999
+      const response = await axios({
+          url: `http://localhost:8000/hotels/${id}`,
+          method: 'DELETE',
+          validateStatus: () => true
+      })
+  
+      expect(response.status).toEqual(404)
+      expect(response.data.message).toBe('Hotel não encontrado')
+    })
 })
